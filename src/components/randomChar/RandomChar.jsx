d7cabdd9722e5216c2d2b775f59f312153986db2
Adiar <datkaevadiar@gmail.com>
2023-11-08 01:01:21 +0600

    Refactoring code and adding custom hooks

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import './randomChar.scss';
@@ -7,34 +7,27 @@ import mjolnir from '../../assets/img/mjolnir.png';
 
 const RandomChar = () => {
 	const [char, setChar] = useState({});
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(false);
 
-	const marvelService = new MarvelService();
+	const { loading, error, getCharacter, clearError } = useMarvelService();
 
 	useEffect(() => {
 		updateChar();
-	}, []);
 
-	const onError = () => {
-		setLoading(false);
-		setError(true);
-	};
+		const timerId = setInterval(updateChar, 60000);
+
+		return () => {
+			clearInterval(timerId);
+		};
+	}, []);
 
 	const onCharLoaded = char => {
 		setChar(char);
-		setLoading(false);
-	};
-
-	const onCharLoading = () => {
-		setLoading(true);
-		setError(false);
 	};
 
 	const updateChar = () => {
+		clearError();
 		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-		onCharLoading();
-		marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
+		getCharacter(id).then(onCharLoaded);
 	};
 
 	const errorMessage = error ? <ErrorMessage /> : null;
--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -1,45 +1,42 @@
-class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=fcdb2bfcdfaed7af723ea8764bec1190';
-    _baseOffset = 210;
+import { useHttp } from '../hooks/http.hook';
 
-    getResource = async (url) => {
-        let res = await fetch(url);
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
-        }
+const useMarvelService = () => {
+	const { loading, request, error, clearError } = useHttp();
 
-        return await res.json();
-    }
+	const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+	const _apiKey = 'apikey=fcdb2bfcdfaed7af723ea8764bec1190';
+	const _baseOffset = 210;
 
-    getAllCharacters = async (offset = this._baseOffset) => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
-        return res.data.results.map(this._transformCharacter);
-    }
+	const getAllCharacters = async (offset = _baseOffset) => {
+		const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+		return res.data.results.map(_transformCharacter);
+	};
 
-    getCharacter = async (id) => {
-        const res = await this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
-        return this._transformCharacter(res.data.results[0]);
-    }
+	const getCharacter = async id => {
+		const res = await request(`${_apiBase}characters/${id}?&${_apiKey}`);
+		return _transformCharacter(res.data.results[0]);
+	};
 
-    _transformCharacter = (char) => {
-        const description = (descr, num) => {
-            if (descr) {
-                return descr.slice(0, num) + '...';
-            } else {
-                return "Sorry, we don't have a description for this character.";
-            }
-        }
-        return {
-            id: char.id,
-            name: char.name,
-            description: description(char.description, 210),
-            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
-            homepage: char.urls[0].url,
-            wiki: char.urls[1].url,
-            comics: char.comics.items,
-        }
-    }
-}
+	const _transformCharacter = char => {
+		const description = (descr, num) => {
+			if (descr) {
+				return descr.slice(0, num) + '...';
+			} else {
+				return "Sorry, we don't have a description for this character.";
+			}
+		};
+		return {
+			id: char.id,
+			name: char.name,
+			description: description(char.description, 210),
+			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+			comics: char.comics.items,
+		};
+	};
 
-export default MarvelService;
+	return { loading, error, getAllCharacters, getCharacter, clearError };
+};
+
+export default useMarvelService;
